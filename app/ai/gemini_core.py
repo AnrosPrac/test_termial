edcd7aeb5a6a87ab57fd6fde0a453d1019b049de
AnrosPrac <sidhiqa20@gmail.com>
2026-01-17 11:59:12 +0530

    first commit

--- app/ai/gemini_core.py
+++ app/ai/gemini_core.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 # Configure Gemini
 genai.configure(api_key=os.getenv("GEMINI-KEY"))
 
-MODEL = genai.GenerativeModel("gemini-2.0-flash-lite")
+MODEL = genai.GenerativeModel("gemini-2.5-flash-lite")
 def run_gemini(prompt: str) -> str:
     response = MODEL.generate_content(prompt)
     return response.text.strip()
